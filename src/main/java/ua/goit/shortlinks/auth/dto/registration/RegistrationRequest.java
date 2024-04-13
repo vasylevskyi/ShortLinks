8e58140a521431f588e2d9dd1e782322c4a59165
Evgeniya <evg.pochitaeva@gmail.com>
2024-04-13 16:52:10 +0300

    changed auth - check email and password correctly

--- src/main/java/ua/goit/shortlinks/auth/dto/registration/RegistrationRequest.java
+++ src/main/java/ua/goit/shortlinks/auth/dto/registration/RegistrationRequest.java
@@ -3,7 +3,6 @@ package ua.goit.shortlinks.auth.dto.registration;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
