28be9657d88a673b0a3667514caa4916bce950a5
vasylevskyi <vasilevskiy@gmail.com>
2024-04-21 16:24:25 +0300

    Added flyway.locations into application-dev.properties, application-prod.properties; Created folders db/migration/prod and db/migration/dev

---
+++ src/test/java/ua/goit/shortlinks/links/LinkValidatorTest.java
@@ -0,0 +1,15 @@
+package ua.goit.shortlinks.links;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class LinkValidatorTest {
+
+    @Test
+    public void testIsLinkValid() {
+        boolean isValid = LinkValidator.isLinkValid("http://valid.link");
+
+        assertTrue(isValid);
+    }
+}
