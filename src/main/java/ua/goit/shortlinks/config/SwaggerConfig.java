78a3b05f398d6b2b97efe29fdf7dc14b005d48d1
Evgeniya <evg.pochitaeva@gmail.com>
2024-04-22 22:03:40 +0300

    fix SwaggerConfig

--- src/main/java/ua/goit/shortlinks/config/SwaggerConfig.java
+++ src/main/java/ua/goit/shortlinks/config/SwaggerConfig.java
@@ -3,34 +3,51 @@ package ua.goit.shortlinks.config;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+                .name("bearerAuth")
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+        return new OpenAPI()
+                .addSecurityItem(securityRequirement)
+                .components(new io.swagger.v3.oas.models.Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .info(new Info().title("Short Links API").version("1.0"));
+    }
+
     @Bean
     public GroupedOpenApi authOpenApi() {
         return GroupedOpenApi.builder()
                 .group("Auth")
-                .pathsToMatch("/auth/***")
+                .pathsToMatch("/api/v1/auth/**")
                 .build();
     }
+
     @Bean
-    public GroupedOpenApi LinkOpenApi() {
+    public GroupedOpenApi linkOpenApi() {
         return GroupedOpenApi.builder()
-                .group("LinkController")
-                .pathsToMatch("/links/**")
-                .pathsToExclude("/links/search/**")
+                .group("Links")
+                .pathsToMatch("/api/v1/links/**")
                 .build();
     }
-    @Bean
-    public GroupedOpenApi RedirectOpenApi() {
+
+/*    @Bean
+    public GroupedOpenApi redirectOpenApi() {
         return GroupedOpenApi.builder()
-                .group("RedirectController")
+                .group("Redirect")
                 .pathsToMatch("/{shortLink}")
                 .build();
-    }
-
-
+    }*/
 }
-
-
-
