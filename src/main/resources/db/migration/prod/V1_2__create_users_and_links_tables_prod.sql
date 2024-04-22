0aa2088e42cc86ce6672683ab5ff32c1a562621d
Evgeniya <evg.pochitaeva@gmail.com>
2024-04-23 00:34:58 +0300

    fix migration fails and add docker

--- src/main/resources/db/migration/prod/V1_2__create_users_and_links_tables_prod.sql
+++ src/main/resources/db/migration/prod/V1_2__create_users_and_links_tables_prod.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 CREATE TABLE IF NOT EXISTS links (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    id BIGSERIAL PRIMARY KEY,
     user_id VARCHAR(100) NOT NULL,
     short_link VARCHAR(50) NOT NULL UNIQUE,
     original_link VARCHAR(1000) NOT NULL,
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS links (
     FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
-INSERT INTO users (user_id, password_hash) VALUES ('john.doe@mail.com', '$2a$12$abMl2.dUx57LDUqhbAh4aeAcsz/iNoX1jebmfSa40AJeC5pbR545i');
-
-INSERT INTO links (user_id, short_link, original_link) VALUES
-    ('john.doe@mail.com', 'ABCD1234', 'https://www.md5hashgenerator.com/');
+--INSERT INTO users (user_id, password_hash) VALUES ('john.doe@mail.com', '$2a$12$abMl2.dUx57LDUqhbAh4aeAcsz/iNoX1jebmfSa40AJeC5pbR545i');
+--
+--INSERT INTO links (user_id, short_link, original_link) VALUES
+--    ('john.doe@mail.com', 'ABCD1234', 'https://www.md5hashgenerator.com/');
