2a21981fa42ec3cd98a3e89a38b2fef7422684b9
Evgeniya <evg.pochitaeva@gmail.com>
2024-04-23 18:47:15 +0300

    fix migration and fix docker

--- src/main/java/ua/goit/shortlinks/links/LinkController.java
+++ src/main/java/ua/goit/shortlinks/links/LinkController.java
@@ -1,5 +1,6 @@
 package ua.goit.shortlinks.links;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -18,22 +19,24 @@ import java.security.Principal;
 @RequestMapping("/api/v1/links")
 public class LinkController {
     private final LinkService linkService;
-
+    @Operation(summary = "Create Link", description = "Creates Link")
     @PostMapping
     public CreateLinkResponse create(Principal principal, @RequestBody CreateLinkRequest request) {
         return linkService.create(principal.getName(), request);
     }
+    @Operation(summary = "Get all user links", description = "Get all user links")
     @GetMapping("/getUserLinks")
     public GetUserLinksResponse getUserLinks(Principal principal) {
         String username = principal.getName();
         return linkService.getUserLinks(username);
     }
-
+    @Operation(summary = "Update link", description = "Update link")
     @PatchMapping
     public UpdateLinkResponse update(Principal principal, @RequestBody UpdateLinkRequest request) {//АПДЕЙТ
         return linkService.update(principal.getName(), request);
     }
 
+    @Operation(summary = "Delete link", description = "Delete link")
     @DeleteMapping("/{shortLink}")
     public DeleteLinkResponse delete(Principal principal, @PathVariable String shortLink) {
         return linkService.delete(principal.getName(), shortLink);
